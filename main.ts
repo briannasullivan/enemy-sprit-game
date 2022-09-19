b73935e8afa2ca1985aac2f01e9a5db858299d62
briannasullivan <113055705+briannasullivan@users.noreply.github.com>
2022-09-19 16:04:26 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    Player.change(LedSpriteProperty.Y, -1)
+})
 input.onGesture(Gesture.TiltLeft, function () {
     Player.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Player.change(LedSpriteProperty.Y, 1)
 })
 input.onGesture(Gesture.TiltRight, function () {
     Player.change(LedSpriteProperty.X, 1)
@@ -8,5 +14,10 @@ let Player: game.LedSprite = null
 Player = game.createSprite(0, 0)
 let Enemy = game.createSprite(randint(0, 4), randint(0, 4))
 basic.forever(function () {
-	
+    if (Player.isTouching(Enemy)) {
+        basic.showIcon(IconNames.Yes)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
+        basic.pause(5000)
+        control.reset()
+    }
 })
